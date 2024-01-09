2e9eed33a9d2b1384a18851b490ca7db1e554aba
leonlux <leonlux@icloud.com>
2024-01-09 08:39:14 +0100

    add stub for RBTree class

---
+++ src/main/java/RBTree.java
@@ -0,0 +1,14 @@
+/**
+ * RBTree represents a Red-Black-Tree, a binary search tree that allows
+ * fast access to stored elements. This is accomplished by following five rules:
+ * 1. Every Node is either red or black
+ * 2. The root of the tree is black
+ * 3. All null-leafs are black
+ * 4. A red node must not have red children
+ * 5. All paths from a node to a leaf contain the same amount of black nodes
+ */
+public class RBTree<T extends Comparable<T>> {
+    public RBTree() {}
+
+    public void insert(T value) {}
+}
